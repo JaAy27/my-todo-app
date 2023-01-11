91ee57999f5bf99ed896774fc30451b714aa0d84
JaAy27 <jnax27@hotmail.com>
2023-01-11 14:21:36 -0500

    Add About page and change the page configuration

--- Home.py
+++ Home.py
@@ -3,6 +3,7 @@ import functionstodo
 
 todos = functionstodo.get_todos()
 
+st.set_page_config(layout="wide")
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
@@ -10,7 +11,8 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("This app is to increase your productivity.")
+st.write("This app is to increase your <b>productivity</b>.",
+         unsafe_allow_html=True)
 
 st.checkbox("Buy grocery.")
 
